82a2c752f9ca2a6b8e6441125c7cf089100cdb7d
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-03 13:57:35 -0500

    Fix issue to createGrid

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import { createGrid } from './modules/createGrid.js';
-import { getComics } from './modules/getData.js';
+import { getData } from './modules/getData.js';
 import { showpopUp } from './modules/popUp.js';
 import { getDataLocalStorage, setDataLocalStorage } from './modules/store.js';
 import './styles.scss';
 
+const container = document.querySelector('.container');
+
 window.addEventListener('load', () => {
-  getComics().then((result) => {
-    result.splice(7, 1);
-    setDataLocalStorage(result);
-  });
-  const mainGrid = getDataLocalStorage();
-  createGrid(mainGrid);
-  const container = document.querySelector('.container');
+  getData();
   container.addEventListener('click', (e) => showpopUp(e));
   const close = document.querySelector('.fa-times');
   close.addEventListener('click', () => {
