840178fe1770fe8bf9149abdebc9ce4a6a8de3f8
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-03 17:06:54 -0500

    Fix popup styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { getData } from './modules/getData.js';
-import { showpopUp } from './modules/popUp.js';
+import { fillPopUp, closePopUp } from './modules/popUp.js';
 import './styles.scss';
 
 const container = document.querySelector('.container');
+const close = document.querySelector('.close');
 
 window.addEventListener('load', () => {
   getData();
-  container.addEventListener('click', (e) => showpopUp(e));
-  const close = document.querySelector('.fa-times');
-  close.addEventListener('click', () => {
-    document.querySelector('section').style.display = 'none';
-  });
 });
+
+container.addEventListener('click', (e) => fillPopUp(e));
+
+close.addEventListener('click', closePopUp);
--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,16 +1,30 @@
 import { getDataLocalStorage } from './store.js';
 
-export function showpopUp(event) {
-  document.querySelector('section').style.display = 'block';
-  if (event.target.classList.contains('button')) {
-    const { id } = event.target.dataset;
+const image = document.querySelector('.image');
+const title = document.querySelector('.modal-title');
+const paragraph = document.querySelector('.dsc');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+const openPopUp = () => {
+  overlay.classList.add('active');
+  modal.classList.add('active');
+}
+
+export const fillPopUp = (event) => {
+  openPopUp();
+  let isButton = event.target.classList.contains('button');
+  if (isButton) {
+    const id = event.target.dataset.id;
     const mainGrid = getDataLocalStorage();
     const element = mainGrid[id];
-    const image = document.querySelector('.image');
-    const title = document.querySelector('.title');
-    const paragraph = document.querySelector('.par');
     image.setAttribute('src', `${element.show.image.original}`);
     title.textContent = `${element.show.name}`;
     paragraph.innerHTML = `${element.show.summary}`;
   }
+}
+
+export const closePopUp = () => {
+  overlay.classList.remove('active');
+  modal.classList.remove('active');
 }
