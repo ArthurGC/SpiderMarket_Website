37d027025b9909c16175ac05cd4e0fa2bbc34529
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-03 11:03:34 -0500

    Fix simplicity of getData function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import { createGrid } from './modules/createGrid.js';
-import { getComics } from './modules/getData.js';
+import { mainGrid } from './modules/getData.js';
 
-window.addEventListener('load', (event)=> {
-    getComics().then( result => {
-        console.log(result);
-        createGrid(result);
-    })
+window.addEventListener('load', ()=> {
+    createGrid(mainGrid)
 })
