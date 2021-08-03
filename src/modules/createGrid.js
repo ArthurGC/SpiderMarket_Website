fafbd61673c039661d344a0ace8af52b24284a49
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-03 10:52:14 -0500

    Add grid of comics

---
+++ src/modules/createGrid.js
@@ -0,0 +1,31 @@
+export const createGrid = (arrayComics) => {
+    let containerJS = document.createDocumentFragment();
+    let mainContainer = document.querySelector('.container');
+    arrayComics.splice(7,1); //Short fixed to get right info
+
+    arrayComics.forEach((comic,index) => {
+        let container = document.createElement('li');
+        let imgContainer = document.createElement('div');
+        let image = document.createElement('img');
+        let likeBox = document.createElement('div');
+        let name = document.createElement('p');
+        let likeIcon = document.createElement('i');
+        let likeAmount = document.createElement('p');
+        let button = document.createElement('button');
+
+        image.setAttribute('src', `${comic.show.image.original}`);
+        name.textContent = `${comic.show.name} ${index}`;
+        likeAmount.textContent = 0;
+        button.textContent = 'Comments';
+
+        imgContainer.appendChild(image);
+        likeBox.appendChild(name);
+        likeBox.appendChild(likeIcon);
+        likeBox.appendChild(likeAmount);
+        container.appendChild(imgContainer);
+        container.appendChild(likeBox);
+        container.appendChild(button);
+        containerJS.appendChild(container);
+    });
+    mainContainer.appendChild(containerJS);
+}
