0492a4832c9349767c3ca021b932db663dcf36e8
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-03 10:52:34 -0500

    Show API info to DOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { createGrid } from './modules/createGrid.js';
 import { getComics } from './modules/getData.js';
 
 window.addEventListener('load', (event)=> {
     getComics().then( result => {
         console.log(result);
+        createGrid(result);
     })
 })
