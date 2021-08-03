0a40a272a57bd253e77c7d661472e4a0a952d285
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-03 11:04:20 -0500

    Fix getData function to get an array of data

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,7 +1,12 @@
 let dataURL = 'https://api.tvmaze.com/search/shows?q=spiderman';
-
-export const getComics = async () => {
+export let mainGrid;
+const getComics = async () => {
     const response = await fetch(dataURL);  
     const comics = await response.json();
     return comics;
 };
+
+getComics().then( result => {
+    result.splice(7,1); 
+    mainGrid = result;
+})
