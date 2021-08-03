a84c81091dfd80f8d1dd3d3a1843ac45fda7a695
Ricardo <rr155914@gmail.com>
2021-08-03 12:30:07 -0500

    Add function popup

--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,5 +1,14 @@
-function showpopUp (event) {
-    if (event.target.classlist.contains("button")) {
+export function showpopUp (event) {
+    if (event.target.classList.contains("button")) {
         let id = event.target.dataset.id
+        let mainGrid = getDataLocalStorage();
+        let element = mainGrid[id];
+        let image = document.querySelector(".image");
+        let title = document.querySelector(".title");
+        let paragraph = document.querySelector(".par");
+        image.setAttribute("src", `${element.show.image.original}`);
+        title.textContent = `${element.show.name}`; 
+        paragraph.textContent = `${element.show.summary}`; 
+
     }
 }
