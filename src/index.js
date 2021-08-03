247caa9aaac25a87a3fe3f056871f48fb983ffee
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-03 13:17:27 -0500

    Fix indentation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import { createGrid } from './modules/createGrid.js';
-import { getComics } from './modules/getData.js'
+import { getComics } from './modules/getData.js';
 import { showpopUp } from './modules/popUp.js';
 import { getDataLocalStorage, setDataLocalStorage } from './modules/store.js';
-import './styles.scss'; 
+import './styles.scss';
 
-window.addEventListener('load', ()=> {
-    getComics().then( result => {
-        result.splice(7,1); 
-        setDataLocalStorage(result);
-
-    })
-    let mainGrid = getDataLocalStorage();
-    createGrid(mainGrid);
-    let container = document.querySelector(".container");
-    container.addEventListener('click', (e) => showpopUp(e));
-    let x = document.querySelector(".X");
-    x.addEventListener('click', () => {
-        document.querySelector("section").style.display = "none";   
-    })
-})
+window.addEventListener('load', () => {
+  getComics().then((result) => {
+    result.splice(7, 1);
+    setDataLocalStorage(result);
+  });
+  const mainGrid = getDataLocalStorage();
+  createGrid(mainGrid);
+  const container = document.querySelector('.container');
+  container.addEventListener('click', (e) => showpopUp(e));
+  const x = document.querySelector('.X');
+  x.addEventListener('click', () => {
+    document.querySelector('section').style.display = 'none';
+  });
+});
--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,17 +1,16 @@
-import { getDataLocalStorage} from "./store.js"
+import { getDataLocalStorage } from './store.js';
 
-export function showpopUp (event) {
-    document.querySelector("section").style.display = "block";
-    if (event.target.classList.contains("button")) {
-        let id = event.target.dataset.id
-        let mainGrid = getDataLocalStorage();
-        let element = mainGrid[id];
-        let image = document.querySelector(".image");
-        let title = document.querySelector(".title");
-        let paragraph = document.querySelector(".par");
-        image.setAttribute("src", `${element.show.image.original}`);
-        title.textContent = `${element.show.name}`; 
-        paragraph.innerHTML = `${element.show.summary}`; 
-
-    }
+export function showpopUp(event) {
+  document.querySelector('section').style.display = 'block';
+  if (event.target.classList.contains('button')) {
+    const { id } = event.target.dataset;
+    const mainGrid = getDataLocalStorage();
+    const element = mainGrid[id];
+    const image = document.querySelector('.image');
+    const title = document.querySelector('.title');
+    const paragraph = document.querySelector('.par');
+    image.setAttribute('src', `${element.show.image.original}`);
+    title.textContent = `${element.show.name}`;
+    paragraph.innerHTML = `${element.show.summary}`;
+  }
 }
