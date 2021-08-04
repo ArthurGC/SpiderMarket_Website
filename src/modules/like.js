4a527e65424de272d8a31f9e9c2c33f425695704
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-04 12:07:09 -0500

    Add execution to getListLikesAPI when add new like

--- src/modules/like.js
+++ src/modules/like.js
@@ -1,14 +1,16 @@
-const createURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-const likeURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1skhHeMuaX5lQlDsoElJ/likes/';
+const createURL =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/";
+const likeURL =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1skhHeMuaX5lQlDsoElJ/likes/";
 
 export const createApp = async () => {
   const response = await fetch(createURL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-type': 'application/json',
+      "Content-type": "application/json",
     },
     body: JSON.stringify({
-      name: 'newAppArthur',
+      name: "newAppArthur",
     }),
   });
   const status = await response.text();
@@ -18,9 +20,9 @@ export const createApp = async () => {
 //   1skhHeMuaX5lQlDsoElJ
 const createLikes = async (id) => {
   const response = await fetch(likeURL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-type': 'application/json',
+      "Content-type": "application/json",
     },
     body: JSON.stringify({
       item_id: `${id}`,
@@ -31,24 +33,30 @@ const createLikes = async (id) => {
 };
 
 export const sendLikes = (event) => {
-  const isLikeBtn = event.target.classList.contains('icon-heart');
+  const isLikeBtn = event.target.classList.contains("icon-heart");
   if (isLikeBtn) {
     const { id } = event.target.dataset;
-    createLikes(id);
+    createLikes(id).then(result => getListLikesAPI());
   }
 };
 
 const getLikes = async () => {
-    const response = await fetch(likeURL);
-  
-    const status = await response.json();
-    return status;
+  const response = await fetch(likeURL);
+
+  const status = await response.json();
+  return status;
 };
 
 const distributeLikes = (likesListAPI) => {
-    let likeCounter = [...document.querySelectorAll('.likes')];
-    likesListAPI.forEach( object => {
-        let id = parseInt(object.item_id,10);
-        likeCounter[id].textContent = object.likes;
-    });
-}
+  let likeCounter = [...document.querySelectorAll(".likes")];
+  likesListAPI.forEach((object) => {
+    let id = parseInt(object.item_id, 10);
+    likeCounter[id].textContent = object.likes;
+  });
+};
+
+export const getListLikesAPI = () => {
+  getLikes().then((result) => {
+    distributeLikes(result);
+  });
+};
