180588fc482dd754092c84f65dd225d6735780db
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-04 12:00:02 -0500

    Fix indentation

--- src/modules/like.js
+++ src/modules/like.js
@@ -1,16 +1,14 @@
-let createURL =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/";
-let likeURL =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1skhHeMuaX5lQlDsoElJ/likes/";
+const createURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const likeURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1skhHeMuaX5lQlDsoElJ/likes/';
 
 export const createApp = async () => {
   const response = await fetch(createURL, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-type": "application/json",
+      'Content-type': 'application/json',
     },
     body: JSON.stringify({
-      name: "newAppArthur",
+      name: 'newAppArthur',
     }),
   });
   const status = await response.text();
@@ -20,9 +18,9 @@ export const createApp = async () => {
 //   1skhHeMuaX5lQlDsoElJ
 const createLikes = async (id) => {
   const response = await fetch(likeURL, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-type": "application/json",
+      'Content-type': 'application/json',
     },
     body: JSON.stringify({
       item_id: `${id}`,
@@ -33,9 +31,9 @@ const createLikes = async (id) => {
 };
 
 export const sendLikes = (event) => {
-    const isLikeBtn = event.target.classList.contains('icon-heart');
-    if (isLikeBtn) {
-        const { id } = event.target.dataset;
-        createLikes(id);
-    }
-}
+  const isLikeBtn = event.target.classList.contains('icon-heart');
+  if (isLikeBtn) {
+    const { id } = event.target.dataset;
+    createLikes(id);
+  }
+};
