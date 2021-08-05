8de8cfd3eb388f1e6b40ad2f8360281ccec23205
Ricardo <rr155914@gmail.com>
2021-08-05 17:09:59 -0500

    check linters

--- src/__test__/Popuptemplate.js
+++ src/__test__/Popuptemplate.js
@@ -5,4 +5,4 @@ export const PopUpTemplate = '<ul class="comments-list">'
 + '<li class="item">'
 + '<p></p>'
 + '</li>'
-+ '</ul>'
++ '</ul>';
--- src/__test__/commentCounter.test.js
+++ src/__test__/commentCounter.test.js
@@ -2,20 +2,19 @@
  * @jest-environment jsdom
  */
 
- import { expect, test } from '@jest/globals';
- import { PopUpTemplate } from './Popuptemplate.js';
- import itemCounterComments from '../modules/commentCounter.js';
- 
- describe('Test ItemCounter', () => {
-   // Arrange
-   document.body.innerHTML = PopUpTemplate;
- 
-   // Act
-   const items = itemCounterComments();
- 
-   // Assert
-   test('itemCounter should return 2', () => {
-     expect(items).toBe(2);
-   });
- });
- 
+import { expect, test } from '@jest/globals';
+import { PopUpTemplate } from './Popuptemplate.js';
+import itemCounterComments from '../modules/commentCounter.js';
+
+describe('Test ItemCounter', () => {
+  // Arrange
+  document.body.innerHTML = PopUpTemplate;
+
+  // Act
+  const items = itemCounterComments();
+
+  // Assert
+  test('itemCounter should return 2', () => {
+    expect(items).toBe(2);
+  });
+});
